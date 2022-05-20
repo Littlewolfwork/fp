91d086c06697c1dc98efa1c7d48fd5123f7ff0df
Еремеев Дмитрий Александрович <U11544@creditural.local>
2022-05-20 15:01:46 +0500

    lesson 11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,58 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-import {findAllByDisplayValue} from "@testing-library/react";
+import Header from './Header.js';
+import Technologies from './Technologies.js';
 
 const App = () => {
   return (
-      <div>
-        <Header />
-        <Technologies />
+      <div className='app-wrapper'>
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/ru/c/cf/%D0%9B%D0%BE%D0%B1%D0%BE%D1%81_%D0%A3%D0%9F%D0%9D%D0%A4%D0%9C_%28%D0%BB%D0%BE%D0%B3%D0%BE%29.png" alt=""/>
+        </header>
+        <nav className='nav'>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
+        </nav>
+          <div className='content'>
+              <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2147483647&v=beta&t=MpzHeo7wdMoePy-CjWNPwwMbgDU3ydtdqIXGYFtSisg" alt=""/>
+          </div>
+
+
       </div>
   );
 }
 
-const Header = () => {
-  return (
-      <div>
-        <a href="">Home</a>
-        <a href="">News</a>
-        <a href="">Messages</a>
-      </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>React</li>
-      </ul>
-  );
-}
-
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 export default App;
